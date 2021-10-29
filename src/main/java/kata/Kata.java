fec057a7abd929ffbc65129d8f48464c8edff141
TonyK54 <vbwriter@gmail.com>
2021-10-29 07:48:05 -0400

    First Pass

--- src/main/java/kata/Kata.java
+++ src/main/java/kata/Kata.java
@@ -1,7 +1,26 @@
 package kata;
 
+import java.util.ArrayList;
+
 public class Kata {
-    public String someKataMethod() {
-        return "hello, world!";
+
+    public String getPublish() {
+        return "I love the weather today.";
+    }
+
+    public String[] getTimeline() {
+        String[] timelineText = {"Good game though. (1 minute ago)", "Darn! We lost! (2 minute ago)"};
+        return timelineText;
+    }
+
+    public ArrayList<String> getFollowing() {
+        ArrayList<String> updates = new ArrayList<>();
+        String[] timeline = this.getTimeline();
+        updates.add("Charlie - I'm in New York today! Anyone wants to have a coffee? (15 seconds ago)");
+        updates.add(String.format("Bob - %s", timeline[0]));
+        updates.add(String.format("Bob - %s", timeline[1]));
+        updates.add(String.format("Alice - %s (5 minutes ago)", getPublish()));
+
+        return updates;
     }
 }
--- src/test/java/kata/KataTest.java
+++ src/test/java/kata/KataTest.java
@@ -2,12 +2,35 @@ package kata;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KataTest {
+
     @Test
-    void testSomeKataMethod() {
-        Kata subjectUnderTest = new Kata();
-        assertEquals("hello, world!", subjectUnderTest.someKataMethod());
+    void testPublish() {
+        Kata publishTest = new Kata();
+        assertEquals("I love the weather today.", publishTest.getPublish());
+    }
+
+    @Test
+    void testTimeline() {
+        Kata timelineTest = new Kata();
+        String[] timelineText = timelineTest.getTimeline();
+        assertEquals("Good game though. (1 minute ago)", timelineText[0]);
+        assertEquals("Darn! We lost! (2 minute ago)", timelineText[1]);
+    }
+
+    @Test
+    void testFollowing() {
+        Kata followingTest = new Kata();
+        ArrayList<String> followingUpdates = followingTest.getFollowing();
+
+        assertEquals("Charlie - I'm in New York today! Anyone wants to have a coffee? (15 seconds ago)", followingUpdates.get(0));
+        assertEquals("Bob - Good game though. (1 minute ago)", followingUpdates.get(1));
+        assertEquals("Bob - Darn! We lost! (2 minute ago)", followingUpdates.get(2));
+        assertEquals("Alice - I love the weather today. (5 minutes ago)", followingUpdates.get(3));
+
     }
 }
